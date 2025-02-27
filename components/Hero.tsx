880a3e0cfee4236623fbe15438033e9c7ed64907
KUNAL KUMAR <kunaldevspro@gmail.com>
2025-02-28 02:51:32 +0530

    init: initialised nextjs user frontend part

---
+++ components/Hero.tsx
@@ -0,0 +1,11 @@
+
+export const Hero = () => {
+    return <div className="text-black pt-10">
+        <div className="text-2xl flex justify-center">
+            Welcome Defi-Tasker
+        </div>
+        <div className="text-lg flex justify-center pt-8">
+            ONE STOP SOLUTION TO GET YOUR TASK DONE !!
+        </div>
+    </div>
+}
